350510f17077434c92cf7a18c607de8573c72781
Erik-TS <erik.t3600@hotmail.com>
2023-06-17 20:12:38 -0300

    Install express.

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
+import express from 'express'
+
+const port = 3000
+
+const app = express()
+
+app.listen(port, () => console.log(`Server is running on port ${port}`))
+
 console.log("Hello World!")
