57f5ae44c3ce13db7cfa9a8f15643c40c5ee79ef
Erik-TS <erik.t3600@hotmail.com>
2023-06-21 09:56:00 -0300

    Create Server class and implement singleton pattern.

--- index.ts
+++ index.ts
@@ -4,13 +4,61 @@ import { faker } from "@faker-js/faker"
 
 const port = 3000
 const app = express()
-const pathToDB = "./usersdb.json"
+let registeredUsers: Array<object> = []
 
-fs.access(pathToDB, fs.constants.F_OK)
-    .then(() => console.log("The \"usersdb.json\" already exists."))
-    .catch((err) => createUsersDB(50))
+class Server {
+    private readonly pathToDB = "./usersdb.json"
+    private registeredUsers: Array<object> = new Array(0)
+    private static instance: Server
+    private isReady = false
 
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+    constructor() {
+        this.startDB().then(value => {
+            console.log(value)
+            this.isReady = true
+        })
+    }
+
+    public getRegisteredUsers(): Array<object> { return this.registeredUsers }
+
+    public static getInstance(): Server {
+        if (Server.instance === null || Server.instance === undefined) Server.instance = new Server()
+        return Server.instance
+    }
+
+    public getIsReady(): boolean { return this.isReady }
+
+    public addRegisteredUsers(users: Array<object>) {
+        registeredUsers.push(users)
+    }
+
+    private async startDB() {
+        await this.prepareDB()
+        await this.importRegisteredUsersFromDB()
+        return "The database has been started."
+    }
+
+    private async prepareDB() {
+        await fs.access(this.pathToDB, fs.constants.F_OK)
+            .then(() => console.log("The \"usersdb.json\" already exists."))
+            .catch((err) => createUsersDB(50))
+    }
+
+    private async importRegisteredUsersFromDB() {
+        try {
+            await fs.readFile(this.pathToDB, "utf-8").then(value => {
+                this.registeredUsers = JSON.parse(value)
+            })
+        } catch (error) {
+            console.error(error)
+        }
+    }
+}
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+    const server = Server.getInstance()
+})
 
 async function createUsersDB(quant: number) {
     interface User {
